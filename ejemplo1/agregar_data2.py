e2542808bb71605366519a2431b972fbbcb19c9b
raul8005 <rimedina@utpl.edu.ec>
2025-05-14 09:15:32 -0500

    Cambio ruta

--- ejemplo1/agregar_data2.py
+++ ejemplo1/agregar_data2.py
@@ -1,20 +1,23 @@
 from sqlalchemy.orm import sessionmaker
-
 from crear_base import Saludo
 from configuracion import engine
 
 Session = sessionmaker(bind=engine)
 session = Session()
 
-with open("../Data/saludos_mundos.csv", "r", encoding="utf-8") as archivo:
-	next(archivo)  # Saltar linea por linea del archivo
+
+with open("/home/raul/PlataformasWeb/clase06-1bim-raul8005/ejemplo1/data/saludos_mundo.csv", "r", encoding="utf-8") as archivo:
+    next(archivo)  # Saltar la línea del encabezado
+
     for linea in archivo:
-        partes = linea.strip().split("|") 
-        if len(partes) == 3:
-            mensaje, tipo, origen = partes
-            saludo = Saludo(mensaje=mensaje, tipo=tipo, origen=origen)
+        partes = linea.strip().split("|")  # Separar por "|"
+
+        if len(partes) >= 2:
+            mensaje = partes[0]
+            tipo = partes[1]
+
+            saludo = Saludo(mensaje=mensaje, tipo=tipo)
             session.add(saludo)
 
-
-#Guardar cambios
+# Guardar cambios
 session.commit()
